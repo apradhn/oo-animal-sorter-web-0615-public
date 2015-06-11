12770518f85f687afe8a1393914bbf149c933a93
apradhn <abhijit.pradhan6@gmail.com>
2015-06-11 14:47:33 -0400

    all done

--- lib/animal_sorter.rb
+++ lib/animal_sorter.rb
@@ -12,3 +12,15 @@
 # should include the sea creatures, the second, land animals.
 
 # Read the test suite for an example of a nested array.
+class AnimalSorter 
+  LAND_ANIMALS = ["aardvark", "cat", "elephant"]
+  SEA_ANIMALS = ["marlin", "octopus", "fish"]
+
+  def initialize(arr)
+    @animals = arr
+  end
+
+  def to_a
+    @animals.partition{|animal| SEA_ANIMALS.include?(animal)}
+  end
+end
